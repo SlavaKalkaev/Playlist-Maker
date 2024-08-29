5fe7a5710409afa5c8a5d6f8baf07fb2351dc3f8
slavakalkaev <slava.kalkaev@mail.ru>
2024-08-29 21:51:38 +0300

    Домашнее задание 8 спринт

---
+++ Solution.kt
@@ -0,0 +1,10 @@
+class Solution {
+    fun findTheDifference(s: String, t: String): Char {
+//        var i =0
+//        var j =0
+//        for (letter in 0..s.length){
+//            i++
+        var i = t[t.length-1]
+        return i
+    }
+}
--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -1,20 +1,57 @@
 package com.example.playlistmaker
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val imageSearch = findViewById<Button>(R.id.search_button)
+        val imageMedia = findViewById<Button>(R.id.media_button)
+        val imageSetting = findViewById<Button>(R.id.settings_button)
+
+        imageSetting.setOnClickListener {
+            val displayIntent = Intent(this, SettingsActivity::class.java)
+            startActivity(displayIntent)
+        }
+        imageMedia.setOnClickListener {
+            val displayIntent = Intent(this, MediaActivity::class.java)
+            startActivity(displayIntent)
+        }
+        imageSearch.setOnClickListener {
+            val displayIntent = Intent(this, SearchActivity::class.java)
+            startActivity(displayIntent)
         }
     }
 }
+//@SuppressLint("MissingInflatedId", "ResourceType")
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_main)
+//        val imageButtonSearch = findViewById<Button>(R.id.search_button)
+//        val imageMediaSearch = findViewById<Button>(R.id.media_button)
+//        val imageSettingSearch = findViewById<Button>(R.id.settings_button)
+//        val imageClickListener: View.OnClickListener = object : View.OnClickListener {
+//            override fun onClick(v: View?) {
+//                Toast.makeText(this@MainActivity, "Поиск", Toast.LENGTH_SHORT).show()
+//            }
+//        }
+//        imageButtonSearch.setOnClickListener(imageClickListener)
+//
+//
+//        imageMediaSearch.setOnClickListener {
+//            Toast.makeText(this@MainActivity, "Медиа", Toast.LENGTH_SHORT).show()
+//        }
+//        imageSettingSearch.setOnClickListener {
+//            Toast.makeText(this@MainActivity, "Настройки", Toast.LENGTH_SHORT).show()
+//        }
+//    }
+
