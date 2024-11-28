11b674042070a60d953cab2542146bd4eb3023ed
slavakalkaev <slava.kalkaev@mail.ru>
2024-11-28 17:44:25 +0300

    Домашняя работа №9

--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -1,18 +1,52 @@
 package com.example.playlistmaker
 
+import android.annotation.SuppressLint
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class SettingsActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+
         val settingsBack = findViewById<ImageView>(R.id.settings_back)
         settingsBack.setOnClickListener {
             val displayIntent = Intent(this, MainActivity::class.java)
             startActivity(displayIntent)
+        }
+
+        val shareButton = findViewById<TextView>(R.id.share_pril)
+        shareButton.setOnClickListener {
+            val message = getString(R.string.share_message)
+
+            val shareIntent = Intent(Intent.ACTION_SEND).apply {
+                type = "text/plain"
+                putExtra(Intent.EXTRA_TEXT, message)
+            }
+            startActivity(Intent.createChooser(shareIntent, getString(R.string.share_title)))
+        }
+
+        val supportButton = findViewById<TextView>(R.id.support)
+        supportButton.setOnClickListener {
+            val recipientEmail = getString(R.string.support_email)
+            val subject = getString(R.string.support_subject)
+            val body = getString(R.string.support_body)
+
+            val uri = Uri.parse("mailto:$recipientEmail?subject=${Uri.encode(subject)}&body=${Uri.encode(body)}")
+            val emailIntent = Intent(Intent.ACTION_SENDTO, uri)
+            startActivity(Intent.createChooser(emailIntent, getString(R.string.support_title)))
+        }
+
+        val agreementButton = findViewById<TextView>(R.id.agree)
+        agreementButton.setOnClickListener {
+            val url = getString(R.string.agree_url)
+            val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+            startActivity(browserIntent)
         }
     }
 }
